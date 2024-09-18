56ff3ca940e05a046a1ac3ad80cdfd2758146a9b
Oscar Vega <os_ar1@hotmail.com>
2024-09-17 21:57:51 -0600

    JavaScript concepts such as variables, styles, and console.log

---
+++ js/01.js
@@ -0,0 +1,24 @@
+// Variables 
+ 
+var product = 'Audifonos Gamer'; //Initializing a variable and assigning a value
+
+var available;// Initializing a variable without a value.
+
+product = true; //Reassigning a value to a variable.
+
+available = true;
+
+var product01 = 'Computadora',
+    available01 = 'true',
+    category = 'Computadoras';
+
+//var 1disponible; it's a common error do not use numbers in the beginnig to variable
+var disponible_;
+
+//style for variables
+var name_product = 'Monitor HD';// underscores
+var nameProduct = 'Monitor HD';// Camelcase
+var NameProduct = 'Monitor HD';// Pascal Case
+var nameproduct = 'Monitor HD';// lower Case or it doesn't have name
+
+console.log(product01, available01, category); // Let's show the result of the variables.
