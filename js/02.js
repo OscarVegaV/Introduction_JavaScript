cea17af79cc8c94a6450a5751fb697b7fd35c34d
Oscar Vega <os_ar1@hotmail.com>
2024-09-17 22:04:33 -0600

    JavaScript concepts such as variables with let, and  styles

---
+++ js/02.js
@@ -0,0 +1,24 @@
+//Variables with Let
+
+ 
+let product = 'Audifonos Gamer'; //Initializing a variable and assigning a value
+
+let available;// Initializing a variable without a value.
+
+//product = true; //Reassigning a value to a variable.
+
+available = true;
+
+let product01 = 'Computadora',
+    available01 = 'true',
+    category = 'Computadoras';
+
+//var 1disponible; it's a common error do not use numbers in the beginnig to variable
+let disponible_;
+
+//style for variables
+let name_product = 'Monitor HD';// underscores
+let nameProduct = 'Monitor HD';// Camelcase
+let NameProduct = 'Monitor HD';// Pascal Case
+let nameproduct = 'Monitor HD';// lower Case or it doesn't have name
+
