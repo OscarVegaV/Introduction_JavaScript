4c5bdf89765e2087866762a90cde688e39d2ef6e
Oscar Vega <os_ar1@hotmail.com>
2024-09-19 12:16:12 -0600

    JavaScript concepts like Numbers (addition, subtraction, and multiplication)

---
+++ js/06.js
@@ -0,0 +1,14 @@
+// Numbers
+
+const number01 = 100;
+const number02 = 200;
+const number03 = 7;
+const number04 = 6;
+const number05 = -5;
+// JS can perform addition, subtraction, and multiplication
+console.log(number01 + number02);
+console.log(number01 - number02);
+console.log(number01 * number02);
+console.log(number01 / number02);
+console.log(number03 % number04); // shows us the remainder of the division
+
