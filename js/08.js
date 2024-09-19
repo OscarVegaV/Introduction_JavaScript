102ee1139332aa05c58464ef26c49e825f6bbc07
Oscar Vega <os_ar1@hotmail.com>
2024-09-19 17:43:01 -0600

    JavaScript Concepts like 'control the order of operations'

---
+++ js/08.js
@@ -0,0 +1,38 @@
+// Order of operations
+
+let result01;
+let result02;
+
+result01 = 20 + 30 * 2;
+result02 = (600 + 600) * 1.05;
+
+console.log(result01);
+console.log(result02);
+
+//increases
+
+let score01 = 10;
+let score02 = 10;
+let score03 = 10;
+let score04 = 10;
+let score05 = 10;
+let score06 = 10;
+
+//Increases 1++
+score01++;//11
+++score01;//12
+
+//Decreases 1--
+score03--;//9
+--score04;//9
+
+//
+score05 += 10;//20
+score06 -= 10;//10
+
+console.log("este es el puntaje 1:", score01);
+console.log("este es el puntaje 2:", score02);
+console.log("este es el puntaje 3:", score03);
+console.log("este es el puntaje 4:", score04);
+console.log("este es el puntaje 5:", score05);
+console.log("este es el puntaje 6:", score06);
