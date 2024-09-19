69194845c1906ccaeb4d1c20ff5a304abe89f72e
Oscar Vega <os_ar1@hotmail.com>
2024-09-18 19:07:15 -0600

    JavaScript concepts like STRING

---
+++ js/04.js
@@ -0,0 +1,14 @@
+//// This is a string or text string
+const product01 = 'Monitor de 20" pulgadas'; // It's the most common to use
+const product02 = String("Monitor de 30\" Pulgadas");
+const product03 = new String('monitor de 50 Pulgadas'); //this is a constructor new
+
+console.log(product01)
+console.log(product02)
+console.log(typeof product03)// this is an object typof
+console.log(typeof product02)// this is a string
+// typeof shows us what type a variable could be
+
+
+
+
