76b784c01ab97e7de4742ee8006f1dfda5c5bad3
Oscar Vega <os_ar1@hotmail.com>
2024-09-19 14:43:19 -0600

    Completed exercises with Math object and made final adjustments in HTML and CSS

---
+++ js/07.js
@@ -0,0 +1,38 @@
+// Math object
+// In the browser, there is a global window object. You can access it by typing 'window' in the console, which will show all available options.
+
+let result01;
+let result02;
+let result03;
+let result04;
+let result05;
+let result06;
+let result07;
+let result08;
+let result09;
+let result10;
+
+result01 = Math.PI;
+result02 = Math.round(2.2);
+result03 = Math.ceil(2.2); // Ceil always rounds up to the nearest integer
+result04 = Math.floor(2.2); // Floor always rounds down to the nearest integer
+result05 = Math.sqrt(144); // Sqrt shows us the square root of a number
+result06 = Math.abs(-2.2); // Converts a negative number to a positive number
+result07 = Math.min(3, 5, 1, 8, 2, 10); // Shows us the minimum number from the list
+result08 = Math.max(3, 5, 1, 8, 2, 10); // Returns the maximum number from the list
+result09 = Math.random(); // Returns a random number between 0 and 1
+result10 = Math.floor(Math.random() * 30); // Combines two functions together
+
+
+
+//returns a result into console
+console.log(result01);
+console.log(result02);
+console.log(result03);
+console.log(result04);
+console.log(result05);
+console.log(result06);
+console.log(result07);
+console.log(result08);
+console.log(result09);
+console.log(result10);
