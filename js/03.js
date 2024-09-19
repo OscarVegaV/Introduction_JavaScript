9f318659f2fbba2332341d24013e037fe82f7987
Oscar Vega <os_ar1@hotmail.com>
2024-09-18 18:43:30 -0600

    JavaScript concepts like const and styles

---
+++ js/03.js
@@ -0,0 +1,26 @@
+//Variables with const 
+/*    Cannot Reassigning a value to a variable
+  const should always be initialized with a value.
+
+*/
+ 
+const product = 'Audifonos Gamer'; //Initializing a variable and assigning a value
+
+const available = true; // const should always be initialized with a value.
+
+//product = true; // Cannot Reassigning a value to a variable.
+//available = true;
+
+
+const product01 = 'Computadora',
+    available01 = 'true',
+    category = 'Computadoras';
+
+//const 1disponible; it's a common error do not use numbers in the beginnig to variable
+const disponible_ = true;
+
+//style for const
+const name_product = 'Monitor HD';// underscores
+const nameProduct = 'Monitor HD';// Camelcase
+const NameProduct = 'Monitor HD';// Pascal Case
+const nameproduct = 'Monitor HD';// lower Case or it doesn't have name
