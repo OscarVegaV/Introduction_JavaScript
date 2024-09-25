680fbaa4db8e03df00cca0f149f65fcc4f9ee37b
Oscar Vega <os_ar1@hotmail.com>
2024-09-24 19:18:47 -0600

    JavaScript concets 'Booleans'

---
+++ js/10.js
@@ -0,0 +1,16 @@
+// Booleans
+
+const boolean1 = true;
+const boolean2 = false;
+const boolean3 = "false";//string
+
+console.log(boolean1);
+console.log(typeof boolean1); //boolean
+console.log(boolean2);
+console.log(boolean3);//string
+console.log(typeof boolean3);//string
+
+//another way to work with boolean
+const boolean04 = new Boolean(true);
+
+console.log(typeof boolean04);//object
