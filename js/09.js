030d5c86f5840d9a088d65c9cad47f5afeed5bee
Oscar Vega <os_ar1@hotmail.com>
2024-09-24 19:05:55 -0600

    Concatenation and template strings

---
+++ js/09.js
@@ -0,0 +1,10 @@
+const name01 = 'Oscar';
+const email = 'oscar@example.com';
+
+//concatenation
+console.log( "Nombre Cliente: " + name01 + " Email " + email);
+
+
+
+// template Strings  String literal || try to use this one
+console.log(`Nombre Cliente: ${name01} Email: ${email} `);
